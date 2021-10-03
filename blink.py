a1d4bf493b05133a50a890af9586983b83877ee8
wartglbr <citronster@gmail.com>
2021-10-03 14:16:10 +0200

    Cleaned up source

--- blink.py
+++ blink.py
@@ -1,73 +1,58 @@
 #!/bin/env python3
-
-import sys
+"""
+Blink blink ;) ;)
+"""
 import time
 
-led_red_file = "/sys/class/leds/led1/brightness"
-led_green_file = "/sys/class/leds/led0/brightness"
+RED_FILE = "/sys/class/leds/led1/brightness"
+GREEN_FILE = "/sys/class/leds/led0/brightness"
 times = 0
 
-def led_status(stat, led):
-	if (stat == 1):
-		if (led == "red"):
-			f = open(led_red_file, 'w')
-			f.write('1')
-			f.close()
-		elif (led == "green"):
-			f = open(led_green_file, 'w')
-			f.write('1')
-			f.close()
-		else:
-			exit()
-	if (stat == 0):
-		if (led == "red"):
-			f = open(led_red_file, 'w')
-			f.write('0')
-			f.close()
-		elif (led == "green"):
-			f = open(led_green_file, 'w')
-			f.write('0')
-			f.close()
-		else:
-			exit()
+
+def write(file, res):
+    if file == "red":
+        f = open(RED_FILE, 'w')
+        f.write(res)
+        f.close()
+    if file == "green":
+        f = open(GREEN_FILE, 'w')
+        f.write(res)
+        f.close
+
 
 def led_on(led):
-	if (led == "red"):
-		f = open(led_red_file, 'w')
-		f.write('1')
-		f.close()
-	elif (led == "green"):
-		f = open(led_green_file, 'w')
-		f.write('1')
-		f.close()
-	else:
-		print("Wrong led")
-		exit()
+    # Turn on led
+    if (led == "red"):
+        write("red", "1")
+    elif (led == "green"):
+        write("green", "1")
+    else:
+        print("Wrong led")
+        exit()
+
 
 def led_off(led):
-	if (led == "red"):
-		f = open(led_red_file, 'w')
-		f.write('0')
-		f.close()
-	elif (led == "green"):
-		f = open(led_green_file, 'w')
-		f.write('0')
-		f.close()
-	else:
-		print("Wrong led")
-		exit()
+    # Turn off led
+    if (led == "red"):
+        write("red", "0")
+    elif (led == "green"):
+        write("green", "0")
+    else:
+        print("Wrong led")
+        exit()
 
 
 def led_blink(times, long):
-	for i in range(times):
-		# Green On, Red Off
-		time.sleep(long)
-		led_on('green')
-		led_off('red')
-		# Red On, Green Off
-		time.sleep(long)
-		led_off('green')
-		led_on('red')
+    # Calls led_on/off for [times] every [long]
+    for i in range(times):
+        # Green On, Red Off
+        time.sleep(long)
+        led_on('green')
+        led_off('red')
+        # Red On, Green Off
+        time.sleep(long)
+        led_off('green')
+        led_on('red')
 
 
 led_blink(5, 0.4)
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/env sh
-ssh pipub -p2280 "python3 "< ./blink.py
+ssh pi -p2280 "python3 "< ./blink.py
